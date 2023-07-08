ab68e019c62ef1b5a783cce30a03e055ead20b0b
bobbycyl <bobbycyl@126.com>
2023-07-09 00:09:38 +0800

    refactor: optimize logic

--- clayutil/futil.py
+++ clayutil/futil.py
@@ -41,7 +41,7 @@ class Properties(object):
     def override(self):
         with open(self.__filename, "w", encoding=self.__encoding) as pf:
             for key, value in self.properties.items():
-                if key[0] == "#" and key[1:].isdigit():
+                if key[0] == "#":
                     pf.write("%s" % value)
                 else:
                     if str(value) == "True":
