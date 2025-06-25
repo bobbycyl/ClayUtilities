fb3421f3c955bd58d92ea17ca4f43cd22cd34089
bobbycyl <bobbycyl@126.com>
2025-06-25 23:59:16 +0800

    fix: change to 12334 proxy port as the tests do

--- examples/fcitx5_dict_updater.py
+++ examples/fcitx5_dict_updater.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     }
 
     # 设置代理
-    custom_proxies = {"http": "http://127.0.0.1:2334", "https": "http://127.0.0.1:2334"}
+    custom_proxies = {"http": "http://127.0.0.1:12334", "https": "http://127.0.0.1:12334"}
     Fcitx5DictUpdater(fcitx5_dicts_dict).download_dicts()
 
     # 清理临时目录
