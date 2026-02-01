a40596137e3acdac4eafc146ef31fa7766d6c8de
bobbycyl <bobbycyl@126.com>
2026-02-01 17:42:12 +0800

    test: add more tests for check_duplicate_filename

--- tests/test_check_duplicate_filename.py
+++ tests/test_check_duplicate_filename.py
@@ -6,3 +6,4 @@ def test():
     assert check_duplicate_filename("./test (1).txt") == "./test (1).txt"
     assert check_duplicate_filename("./test (1) (1).txt") == "./test (1) (4).txt"
     assert check_duplicate_filename("./test (999).txt") == "./test (1000).txt"
+    assert check_duplicate_filename("./test_5.test", r"_{n}") == "./test_6.test"
